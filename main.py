406f40abd5f6fbc64daebfc0d49ee73ec2dd46fa
flpflJ <flpflpfli@gmail.com>
2025-04-15 22:16:19 +0700

    CORS fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from core.models import db_helper, Base
@@ -17,6 +18,12 @@ async def lifespan(app:FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(router_v1)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
